dd196d8de71843ec2e257a13e66591349e0fc734
Violetta Prelovskaya <viprelova@gmail.com>
2021-01-18 18:40:22 +0300

    Переделана функция добавления нового таска. Реализован localStorage. Реализован drag and drop в пределах одного столбика

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,82 @@
-document.querySelector('.add-task__button').addEventListener('click', function(evt) {
-    evt.preventDefault();
-    let taskValue = document.querySelector('.task-value').value;
-    console.log(taskValue);
-    addTask(taskValue);
-})
+let addMessage = document.querySelector('#addTask'),
+ addButton = document.querySelector('#addButton'),
+ todo = document.querySelector('.taskboard__list'),
+ tasks = document.querySelectorAll('.task'),
+ editBtn = document.querySelector('.task__edit');
 
-const addTask = (taskValue) => {
-    let task = document.createElement('div');
-    task.classList.add('taskboard__item');
-    task.classList.add('task');
+let todoList = [];
 
-    let taskContent = document.createElement('div');
-    taskContent.classList.add('task__body');
-    taskContent.setAttribute('draggable', 'true');
-
-    let taskText = document.createElement('p');
-    taskText.classList.add('task__view');
-    taskText.innerText = taskValue;
-
-    let edit = document.createElement('button');
-    edit.classList.add('task__edit');
-    edit.setAttribute('type', 'button');
-    edit.setAttribute('aria-label', 'Изменить');
-
-    task.appendChild(taskContent);
-    task.appendChild(taskText);
-    task.appendChild(edit);
-
-    taskList = document.querySelector('.taskboard__list');
-    taskList.appendChild(task);
+if(localStorage.getItem('todo')){
+    todoList = JSON.parse(localStorage.getItem('todo'));
+    displayMessages();
 }
+
+addButton.addEventListener('click', function(){
+
+    let newTodo = {
+        todo: addMessage.value,
+    };
+
+    todoList.push(newTodo);
+    localStorage.setItem('todo', JSON.stringify(todoList));
+    if(addMessage.value) displayMessages();
+    addMessage.value = '';
+
+ });
+
+function displayMessages(){
+
+    let displayMessage = '';
+
+    todoList.forEach(function(item, i){
+
+    displayMessage += `
+    <div class="taskboard__item task" draggable='true'>
+        <div class="task__body">
+            <p class="task__view">${item.todo}</p>
+            <input class='task__input' type='text' id='item_${i}'>
+        </div>
+        <button class="task__edit" type="button" aria-label="Изменить"></button>
+    </div>
+    `;
+    todo.innerHTML = displayMessage;
+});
+
+};
+
+const taskElements = todo.querySelectorAll(`.taskboard__item`);
+
+for (const task of taskElements) {
+    task.draggable = true;
+}
+
+todo.addEventListener(`dragstart`, (evt) => {
+    evt.target.classList.add(`task--dragged`);
+});
+  
+todo.addEventListener(`dragend`, (evt) => {
+    evt.target.classList.remove(`task--dragged`);
+});
+
+todo.addEventListener(`dragover`, (evt) => {
+
+    evt.preventDefault();
+  
+    const activeElement = todo.querySelector(`.task--dragged`);
+
+    const currentElement = evt.target;
+
+    const isMoveable = activeElement !== currentElement &&
+      currentElement.classList.contains(`task`);
+  
+    if (!isMoveable) {
+      return;
+    }
+  
+    const nextElement = (currentElement === activeElement.nextElementSibling) ?
+        currentElement.nextElementSibling :
+        currentElement;
+  
+    todo.insertBefore(activeElement, nextElement);
+});
+
